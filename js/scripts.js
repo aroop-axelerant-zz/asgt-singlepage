481f514cf852dae60208845fa43450460ac4bc39
aroop <aroop@axelerant.com>
2019-06-18 16:19:07 +0530

    asgt-fedefaults: Created about section and finished the header styles.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
-if ($(window).width() >= 1200) {
-    $(window).scroll(function (){
-        var header = $(".page-header").height();
-        var scroll = $(window).scrollTop();
-        if(scroll > header) {
-            $('.page-header').css('background', 'black');
+// Header animations on scroll in desktop
+$(window).scroll(function (){
+    var header = $(".page-header").height();
+    var scroll = $(window).scrollTop();
+    if(scroll > header) {
+        $('.page-header').css('background', 'black');
+        if ($(window).width() >= 1200) {
             $('.navigation').css('transform', 'translateX(-40px)');
             $('.primary-cta').css('display', 'block');
-        } else {
-            $('.page-header').css('background', 'none');
+        }
+    } else {
+        $('.page-header').css('background', 'none');
+        if ($(window).width() >= 1200) {
             $('.navigation').css('transform', 'translateX(-100%)');
             $('.primary-cta').css('display', 'none');
         }
-    });
-}
+    }
+});
+
+// Menu Toggle on click
+$('.page-header .ico-menu').click(function (){
+    $('.page-header .navigation').slideToggle();
+});
